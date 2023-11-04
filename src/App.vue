c6998fa67741e60fb376d46d9bc100293171ab5d
VincenzoDePetris <vince02@live.it>
2023-11-04 16:43:23 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import WorkList from "./components/works/WorkList.vue";
+
+export default {
+  data() {
+    return {};
+  },
+
+  components: { WorkList },
+};
+</script>
+
+<template>
+  <h1>BOOLFOLIO</h1>
+  <WorkList />
+</template>
+
+<style lang="scss"></style>
---
+++ src/components/works/WorkList.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <h2>Work List</h2>
+</template>
+
+<style lang="scss"></style>
